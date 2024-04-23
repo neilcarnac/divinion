b17e4f069ff8dacc2c27e05cc18e95a71f6bdf3d
neilo <neil.carnac@somaiya.edu>
2024-04-23 18:23:04 +0530

    feat:ui chnages

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,7 +4,6 @@ import { Route, Routes } from 'react-router';
 import HomePage from './Pages/HomePage';
 import Admin from './Pages/Admin';
 import Login from './Pages/Login';
-import Signup from './Pages/Signup';
 import News from './Pages/News';
 import AdminLogin from './Pages/AdminLogin';
 
@@ -14,7 +13,6 @@ const AppRouter = () => {
             <Route path="/" element={<HomePage />} />        
             <Route path="/admin" element={<Admin />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/Signup" element={<Signup />} />
             <Route path="/blog" element={<News />} />
             <Route path="/adminLogin" element={<AdminLogin />} />
 
