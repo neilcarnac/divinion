ee0a6ed0036ea7b6003bc3414818fa4b555613f0
Neil Carnac <neil@Neils-MacBook-Air-2.local>
2024-12-13 13:35:32 +0530

    ui change

--- src/Components/SPPComponent.jsx
+++ src/Components/SPPComponent.jsx
@@ -18,41 +18,33 @@ const policies = [
 const SPPComponent = () => {
   return (
     <div className="flex flex-col w-full mx-auto">
-       <div className="font-joe">
-      <p className="text-center p-4">
-        {" "}
-        {/* Added padding and centered text */}
-        <a
-          href="https://drive.google.com/drive/folders/1u9_s8i2xLv-P2Knq2vAkHx-syHdxd670?usp=sharing"
-          className="text-blue-500 hover:underline "
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click here
-        </a>{" "}
-        for our Policies
-      </p>
+      <div className="font-joe py-28">
+        <p className="text-center ">
+          {" "}
+          {/* Added padding and centered text */}
+          <a
+            href="https://drive.google.com/drive/folders/1u9_s8i2xLv-P2Knq2vAkHx-syHdxd670?usp=sharing"
+            className="text-blue-500 hover:underline "
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Click here
+          </a>{" "}
+          for our Policies
+        </p>
       </div>
-      <div className="flex flex-col gap-5">
+      {/* <div className="flex flex-col gap-5">
         {policies.map((policy, index) => (
           <div
             key={index}
-            className="p-4 w-3/4 mx-auto flex flex-col items-center shadow-sm hover:shadow-md cursor-pointer transition duration-500 bg-white rounded-md"
+            className="p-4 w-3/4 mx-auto flex flex-col items-center transition duration-500 bg-white rounded-md"
           >
             <div className="flex flex-row justify-between items-center gap-5 w-full mb-4">
               <p className="text-xl leading-tight font-semibold">{policy}</p>
-              <a
-                target="_blank"
-                href="https://drive.google.com/drive/folders/1u9_s8i2xLv-P2Knq2vAkHx-syHdxd670"
-              >
-                <button className="p-3 rounded-full transition duration-500 hover:bg-black hover:text-white">
-                  <FaExternalLinkAlt />
-                </button>
-              </a>
             </div>
           </div>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
